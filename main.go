d6cbeb1ad516a1d83db11cfae864f720ba371077
Calle Pettersson <cpettsson@gmail.com>
2018-07-18 13:18:44 +0200

    Fixes #2. Also refactors a lot, in order to be able to test

--- main.go
+++ main.go
@@ -3,7 +3,7 @@ package main
 import (
 	"flag"
 	"fmt"
-	"io/ioutil"
+	"io"
 	"log"
 	"os"
 	"sort"
@@ -11,6 +11,7 @@ import (
 	"text/template"
 
 	"github.com/miekg/dns"
+	"golang.org/x/net/idna"
 )
 
 // Build information. Populated at build-time.
@@ -22,53 +23,60 @@ var (
 	BuildDate string
 )
 
-const zoneTemplate = `resource "aws_route53_zone" "{{ .Id }}" {
+const (
+	zoneTemplateStr = `resource "aws_route53_zone" "{{ .ID }}" {
    name = "{{ .Domain }}"
 }
 `
-const recordTemplate = `{{- range .Record.Comments }}
+	recordTemplateStr = `{{- range .Record.Comments }}
 # {{ . }}{{ end }}
-resource "aws_route53_record" "{{ .ResourceId }}" {
-   zone_id = "${aws_route53_zone.{{ .Zone.Id }}.zone_id}"
+resource "aws_route53_record" "{{ .ResourceID }}" {
+   zone_id = "${aws_route53_zone.{{ .ZoneID }}.zone_id}"
    name = "{{ .Record.Name }}"
    type = "{{ .Record.Type }}"
-   ttl = "{{ .Record.Ttl }}"
+   ttl = "{{ .Record.TTL }}"
    records = [{{ range $idx, $elem := .Record.Data }}{{ if $idx }},{{ end }}{{ ensureQuoted $elem }}{{ end }}]
 }
 `
+)
 
-type ZoneTemplateData struct {
-	Id     string
+var (
+	zoneTemplate   = template.Must(template.New("zone").Parse(zoneTemplateStr))
+	recordTemplate = template.Must(template.New("record").Funcs(template.FuncMap{"ensureQuoted": ensureQuoted}).Parse(recordTemplateStr))
+)
+
+type zoneTemplateData struct {
+	ID     string
 	Domain string
 }
-type RecordTemplateData struct {
-	ResourceId string
-	Record     *TerraformRecord
-	Zone       ZoneTemplateData
+type recordTemplateData struct {
+	ResourceID string
+	Record     dnsRecord
+	ZoneID     string
 }
-type TerraformRecord struct {
+type dnsRecord struct {
 	Name     string
 	Type     string
-	Ttl      uint32
+	TTL      uint32
 	Data     []string
 	Comments []string
 }
-type RecordKey struct {
+type recordKey struct {
 	Name string
 	Type string
 }
-type RecordKeySlice []RecordKey
+type recordKeySlice []recordKey
 
-func (records RecordKeySlice) Len() int {
+func (records recordKeySlice) Len() int {
 	return len(records)
 }
-func (records RecordKeySlice) Less(i, j int) bool {
-	genKey := func(k RecordKey) string {
+func (records recordKeySlice) Less(i, j int) bool {
+	genKey := func(k recordKey) string {
 		return fmt.Sprintf("%s-%s", k.Name, k.Type)
 	}
 	return genKey(records[i]) < genKey(records[j])
 }
-func (records RecordKeySlice) Swap(i, j int) {
+func (records recordKeySlice) Swap(i, j int) {
 	tmp := records[i]
 	records[i] = records[j]
 	records[j] = tmp
@@ -76,13 +84,12 @@ func (records RecordKeySlice) Swap(i, j int) {
 
 var (
 	excludedTypesRaw = flag.String("exclude", "SOA,NS", "Comma-separated list of record types to ignore")
-	excludedTypes    map[string]bool
 	domain           = flag.String("domain", "", "Name of domain")
 	zoneFile         = flag.String("zone-file", "", "Path to zone file. Defaults to <domain>.zone in working dir")
 	showVersion      = flag.Bool("version", false, "Show version")
 )
 
-func init() {
+func main() {
 	flag.Parse()
 	if *showVersion {
 		fmt.Printf("bzfttr53rdutil %s (%s/%s) (%s on %s)", Version, Branch, Revision, BuildUser, BuildDate)
@@ -96,91 +103,162 @@ func init() {
 		*zoneFile = fmt.Sprintf("%s.zone", *domain)
 	}
 
-	excludedTypes = make(map[string]bool)
-	for _, t := range strings.Split(*excludedTypesRaw, ",") {
-		excludedTypes[strings.ToUpper(t)] = true
+	excludedTypes := excludedTypesFromString(*excludedTypesRaw)
+
+	fileReader, err := os.Open(*zoneFile)
+	if err != nil {
+		log.Fatal(err)
 	}
+
+	generateTerraformForZone(*domain, excludedTypes, fileReader, os.Stdout)
 }
 
-func main() {
-	bytes, err := ioutil.ReadFile(*zoneFile)
+func generateTerraformForZone(domain string, excludedTypes map[uint16]bool, zoneReader io.Reader, output io.Writer) {
+	records := readZoneRecords(zoneReader, excludedTypes)
+
+	zoneID, err := generateZoneResource(domain, output)
 	if err != nil {
-		log.Panic(err)
-	}
-	zone := string(bytes)
-	reader := strings.NewReader(zone)
-
-	records := make(map[RecordKey]*TerraformRecord)
-	for rr := range dns.ParseZone(reader, *domain, *zoneFile) {
-		if rr.Error != nil {
-			log.Printf("Error: %v\n", rr.Error)
-		} else {
-			header := rr.Header()
-			recordType := dns.Type(header.Rrtype).String()
-			isExcluded, ok := excludedTypes[recordType]
-
-			if ok && isExcluded {
-				continue
-			}
-
-			name := strings.ToLower(header.Name)
-			data := strings.TrimPrefix(rr.String(), header.String())
-			if recordType == "CNAME" {
-				data = strings.ToLower(data)
-			}
-
-			key := RecordKey{
-				Name: name,
-				Type: recordType,
-			}
-			if rec, ok := records[key]; ok {
-				rec.Data = append(rec.Data, data)
-				if rr.Comment != "" {
-					rec.Comments = append(rec.Comments, strings.TrimLeft(rr.Comment, ";"))
-				}
-			} else {
-				comments := make([]string, 0)
-				if rr.Comment != "" {
-					comments = append(comments, strings.TrimLeft(rr.Comment, ";"))
-				}
-				records[key] = &TerraformRecord{
-					Name:     key.Name,
-					Type:     key.Type,
-					Ttl:      header.Ttl,
-					Data:     []string{data},
-					Comments: comments,
-				}
-			}
-		}
+		log.Fatal(err)
 	}
 
-	zoneName := strings.TrimRight(*domain, ".")
-	ZoneTemplateData := ZoneTemplateData{
-		Id:     strings.Replace(zoneName, ".", "-", -1),
-		Domain: zoneName,
-	}
-	terraformZone := template.Must(template.New("zone").Parse(zoneTemplate))
-	terraformZone.Execute(os.Stdout, ZoneTemplateData)
-
-	resource := template.Must(template.New("resource").Funcs(template.FuncMap{"ensureQuoted": ensureQuoted}).Parse(recordTemplate))
-	recordKeys := make(RecordKeySlice, 0, len(records))
-	for key, _ := range records {
+	recordKeys := make(recordKeySlice, 0, len(records))
+	for key := range records {
 		recordKeys = append(recordKeys, key)
 	}
 	sort.Sort(sort.Reverse(recordKeys))
 
 	for _, key := range recordKeys {
 		rec := records[key]
-		hyphenatedName := strings.Replace(strings.TrimRight(rec.Name, "."), ".", "-", -1)
-		wildcardCleanedName := strings.Replace(hyphenatedName, "*", "wildcard", -1)
-		id := fmt.Sprintf("%s-%s", wildcardCleanedName, rec.Type)
-		info := RecordTemplateData{
-			ResourceId: id,
-			Record:     rec,
-			Zone:       ZoneTemplateData,
+		err := generateRecordResource(rec, zoneID, output)
+		if err != nil {
+			log.Printf("Error: %v\n", err)
+			continue
 		}
-		resource.Execute(os.Stdout, info)
 	}
+}
+
+func readZoneRecords(zoneReader io.Reader, excludedTypes map[uint16]bool) map[recordKey]dnsRecord {
+	records := make(map[recordKey]dnsRecord)
+	for rr := range dns.ParseZone(zoneReader, *domain, *zoneFile) {
+		if rr.Error != nil {
+			log.Printf("Error: %v\n", rr.Error)
+			continue
+		}
+
+		recordType := rr.Header().Rrtype
+		isExcluded, ok := excludedTypes[recordType]
+		if ok && isExcluded {
+			continue
+		}
+
+		record := generateRecord(rr)
+
+		key := recordKey{record.Name, record.Type}
+		if _, ok := records[key]; ok {
+			record = mergeRecords(records[key], record)
+		}
+
+		records[key] = record
+	}
+	return records
+}
+
+func generateZoneResource(domain string, w io.Writer) (string, error) {
+	zoneName := strings.TrimRight(domain, ".")
+	data := zoneTemplateData{
+		ID:     strings.Replace(zoneName, ".", "-", -1),
+		Domain: zoneName,
+	}
+
+	err := zoneTemplate.Execute(w, data)
+	return data.ID, err
+}
+
+func generateRecordResource(record dnsRecord, zoneID string, w io.Writer) error {
+	sanitizedName := sanitizeRecordName(record.Name)
+	id := fmt.Sprintf("%s-%s", sanitizedName, record.Type)
+
+	data := recordTemplateData{
+		ResourceID: id,
+		Record:     record,
+		ZoneID:     zoneID,
+	}
+
+	return recordTemplate.Execute(w, data)
+}
+
+func mergeRecords(a, b dnsRecord) dnsRecord {
+	a.Data = append(a.Data, b.Data...)
+	a.Comments = append(a.Comments, b.Comments...)
+
+	return a
+}
+
+func generateRecord(rr *dns.Token) dnsRecord {
+	header := rr.Header()
+	name := strings.ToLower(header.Name)
+
+	key := recordKey{
+		Name: name,
+		Type: dns.TypeToString[header.Rrtype],
+	}
+
+	data := strings.TrimPrefix(rr.String(), header.String())
+	if key.Type == "CNAME" {
+		data = strings.ToLower(data)
+	}
+
+	comments := make([]string, 0)
+	if rr.Comment != "" {
+		comments = append(comments, strings.TrimLeft(rr.Comment, ";"))
+	}
+	return dnsRecord{
+		Name:     key.Name,
+		Type:     key.Type,
+		TTL:      header.Ttl,
+		Data:     []string{data},
+		Comments: comments,
+	}
+}
+
+// sanitizeRecordName creates a normalized record name that Terraform accepts.
+// Terraform only allows letters, numbers, dashes and underscores, while DNS
+// records allow far more.
+// 1. All dots are replaced with -
+// 2. * is replaced by the string "wildcard"
+// 3. IDN records are cleaned using punycode conversion
+// 4. Any remaining non-allowed characters are replaced underscore
+// 5. The record type is appended
+func sanitizeRecordName(name string) string {
+	withoutDots := strings.Replace(strings.TrimRight(name, "."), ".", "-", -1)
+	withoutAsterisk := strings.Replace(withoutDots, "*", "wildcard", -1)
+
+	punycoded, err := idna.Punycode.ToASCII(withoutAsterisk)
+	if err != nil {
+		log.Fatalf("Cannot create resource name from record %s: %v", name, err)
+	}
+
+	id := strings.Map(func(r rune) rune {
+		if (r >= 'a' && r <= 'z') ||
+			(r >= 'A' && r <= 'z') ||
+			(r >= '0' && r <= '9') ||
+			(r == '-' || r == '_') {
+			return r
+		}
+		return '_'
+	}, punycoded)
+
+	return id
+}
+
+func excludedTypesFromString(s string) map[uint16]bool {
+	excludedTypes := make(map[uint16]bool)
+	for _, t := range strings.Split(s, ",") {
+		t = strings.ToUpper(t) // ensure upper case
+		rrType := dns.StringToType[t]
+		excludedTypes[rrType] = true
+	}
+	return excludedTypes
 }
 
 func ensureQuoted(s string) string {
